2b5b4b68b663543614bf3d658af78510bbd27c9f
polakrit-pipi <polakritkrajaisri@gmail.com>
2023-06-16 19:47:06 +0700

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <h1 className="text-5xl font-bold ">
-    poom
+    <div className='flex flex-col gap-10 pt-10'>
+        <h1 className="text-5xl font-bold text-center"> 
+    Ocean Pal
   </h1>
+  <div className='flex flex-col items-center gap-10'>
+  <button className="btn">Hello daisyUI</button>
+  <button className="btn">Hello LazyUI</button>
+
+  </div>
   </div>
   )
 }
