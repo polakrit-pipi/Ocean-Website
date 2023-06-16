1bf21cb164c76e31ddcbfccdab37a926d6518427
polakrit-pipi <polakritkrajaisri@gmail.com>
2023-06-16 18:59:47 +0700

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className="text-5xl font-bold ">
+    Cdlor
+  </h1>
+  </div>
+  )
+}
+
+export default Header
